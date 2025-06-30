c6947f960c4f6fb1d9304c5ba206516c2cd71783
devjeverson <developer.oliveirajv@gmail.com>
2025-06-30 20:21:57 -0300

    feat: update header and footer components with icons and links; adjust home component text and styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
