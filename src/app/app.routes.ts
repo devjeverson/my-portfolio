c0a37a6d135169fd7a5b7122d7650618dc8a9670
devjeverson <developer.oliveirajv@gmail.com>
2025-07-03 16:34:45 -0300

    feat: add new project components and implement routing; enhance project display with detailed views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsFrontComponent } from './projects-front/projects-front.component';
+import { ProjectsBackComponent } from './projects-back/projects-back.component';
+import { ProjectsMobileComponent } from './projects-mobile/projects-mobile.component';
 
 export const routes: Routes = [
-   { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, },
   { path: 'component-front', component: ProjectsFrontComponent },
+  { path: 'component-back', component: ProjectsBackComponent },
+  { path: 'component-mobile', component: ProjectsMobileComponent },
+
 ];
--- src/app/projects-mobile/projects-mobile.component.ts
+++ src/app/projects-mobile/projects-mobile.component.ts
@@ -1,11 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+interface Project {
+  title: string;
+  image: string;
+  summary: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-projects-mobile',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  standalone: true,
   templateUrl: './projects-mobile.component.html',
   styleUrl: './projects-mobile.component.scss'
 })
 export class ProjectsMobileComponent {
+    
+  projects: Project[] = [
+
+   {
+      title: 'Projeto Ampliado',
+      image: 'assets/projeto-ampliado.png',
+      summary: 'Outro projeto incrível feito com Angular e design moderno',
+      link: 'https://github.com/seuusuario/seurepositorio2'
+    },
+     {
+      title: 'Projeto Ampliado',
+      image: 'assets/projeto-ampliado.png',
+      summary: 'Outro projeto incrível feito com Angular e design moderno',
+      link: 'https://github.com/seuusuario/seurepositorio2'
+    },
+     {
+      title: 'Projeto Ampliado',
+      image: 'assets/projeto-ampliado.png',
+      summary: 'Outro projeto incrível feito com Angular e design moderno',
+      link: 'https://github.com/seuusuario/seurepositorio2'
+    },
+     {
+      title: 'Projeto Ampliado',
+      image: 'assets/projeto-ampliado.png',
+      summary: 'Outro projeto incrível feito com Angular e design moderno',
+      link: 'https://github.com/seuusuario/seurepositorio2'
+    },
+     {
+      title: 'Projeto Ampliado',
+      image: 'assets/projeto-ampliado.png',
+      summary: 'Outro projeto incrível feito com Angular e design moderno',
+      link: 'https://github.com/seuusuario/seurepositorio2'
+    },
+     {
+      title: 'Projeto Ampliado',
+      image: 'assets/projeto-ampliado.png',
+      summary: 'Outro projeto incrível feito com Angular e design moderno',
+      link: 'https://github.com/seuusuario/seurepositorio2'
+    }
+  ];
+
+  expandedCard: number | null = null;
+
+  toggleExpand(index: number): void {
+    this.expandedCard = this.expandedCard === index ? null : index;
+  }
+
+  closeCard(event: Event): void {
+    event.stopPropagation(); // evita conflito com clique no card
+    this.expandedCard = null;
+  }
+
+  nextCard(): void {
+    if (this.expandedCard !== null) {
+      this.expandedCard = (this.expandedCard + 1) % this.projects.length;
+    }
+  }
+
+  previousCard(): void {
+    if (this.expandedCard !== null) {
+      this.expandedCard = (this.expandedCard - 1 + this.projects.length) % this.projects.length;
+    }
+  }
 
 }
