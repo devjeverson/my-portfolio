da1f4c2b0ef908e2687f6bf088750247f842b769
devjeverson <developer.oliveirajv@gmail.com>
2025-07-05 14:21:46 -0300

    refactor: remove EmailJS integration and rework form submission to use local server; update contact form layout

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import emailjs from '@emailjs/browser';
 import { NgxCaptchaModule } from 'ngx-captcha';
 
 @Component({
@@ -25,16 +24,8 @@ export class ContactsComponent {
    mensagemEnviada = false;
   captchaValid = false;
 
-  // Substitua com suas chaves EmailJS
-  private SERVICE_ID = 'seu_service_id';
-  private TEMPLATE_ID = 'seu_template_id';
-  private PUBLIC_KEY = 'seu_public_key';
-
-  onSubmit() {
-    if (!this.captchaValid) {
-      alert('Por favor, confirme o CAPTCHA.');
-      return;
-    }
+  /*onSubmit() {
+    
 
     const templateParams = {
       from_name: this.form.nome,
@@ -42,21 +33,23 @@ export class ContactsComponent {
       subject: this.form.assunto,
       message: this.form.mensagem,
     };
+  }*/
 
-    emailjs.send(this.SERVICE_ID, this.TEMPLATE_ID, templateParams, this.PUBLIC_KEY)
-      .then(() => {
-        this.mensagemEnviada = true;
-        this.form = { nome: '', email: '', assunto: '', mensagem: '' };
-        setTimeout(() => this.mensagemEnviada = false, 5000);
-      })
-      .catch(error => {
-        console.error('Erro ao enviar e-mail:', error);
-        alert('Erro ao enviar mensagem. Tente novamente.');
-      });
-  }
+  onSubmit() {
+    fetch('http://localhost:8080/send', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(this.form)
+  })
+  .then(res => {
+    if (res.ok) {
+      this.mensagemEnviada = true;
+      this.form = { nome: '', email: '', assunto: '', mensagem: '' };
+    } else {
+      alert('Erro ao enviar mensagem.');
+    }
+  });
+}
 
-  resolvedCaptcha(token: any): void {
-    this.captchaValid = !token;
-  }
 
 }
